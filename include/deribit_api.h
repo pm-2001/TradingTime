8f9098ea0678f247d2c611418306173634d73d47
pm-2001 <princemauryapm2001@gmail.com>
2024-12-28 23:19:18 +0530

    changes

--- include/deribit_api.h
+++ include/deribit_api.h
@@ -6,22 +6,22 @@
 using namespace std;
 
 class DeribitAPI {
-private:
-    string base_url;
-    string client_id;
-    string client_secret;
-    string access_token;
+    private:
+        string base_url;
+        string client_id;
+        string client_secret;
+        string access_token;
 
-    static size_t WriteCallback(void *contents, size_t size, size_t nmemb, string *output);
+        static size_t WriteCallback(void *contents, size_t size, size_t nmemb, string *output);
 
-public:
-    DeribitAPI(const string &client_id, const string &client_secret);
+    public:
+        DeribitAPI(const string &client_id, const string &client_secret);
 
-    bool authenticate();
-    string getOrderBook(const string &symbol);
-    string getInstruments(const string &currency, const string &kind = "future");
-    string getAccountSummary(const string &currency);
-    string getTicker(const string &symbol);
+        bool authenticate();
+        string getOrderBook(const string &symbol);
+        string getInstruments(const string &currency, const string &kind = "future");
+        string getAccountSummary(const string &currency);
+        string getTicker(const string &symbol);
 };
 
 #endif
